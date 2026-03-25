c1c7265e4db43eac18ae012cff2a9f5ed7835be1
Guilherme Eleuthério <eleutherio.profissional@gmail.com>
2026-03-25 11:05:15 -0300

    Adiciona timeout de rede no login e recuperacao de senha

---
+++ grengame-frontend/src/utils/fetchWithTimeout.ts
@@ -0,0 +1,17 @@
+export async function fetchWithTimeout(
+  input: RequestInfo | URL,
+  init: RequestInit = {},
+  timeoutMs = 15000
+) {
+  const controller = new AbortController();
+  const timeoutId = window.setTimeout(() => controller.abort(), timeoutMs);
+
+  try {
+    return await fetch(input, {
+      ...init,
+      signal: controller.signal,
+    });
+  } finally {
+    window.clearTimeout(timeoutId);
+  }
+}
